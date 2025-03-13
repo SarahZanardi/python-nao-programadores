92b832623eb06f535603a231fc2b7a04e8ec0692
Sarah Zanardi <sarahzanardi6@gmail.com>
2025-03-13 13:39:33 -0300

    Variaveis.py

---
+++ Variaveis.py
@@ -0,0 +1,29 @@
+# Convenções para nomear as variáveis:
+# - Iniciar sempre com letra minúscula;
+# - O nome pode ser composto por letras, algarismos e underlines;
+# - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
+
+# Declare variáveis do tipo string
+nome = 'Paloma Oliveira'
+profissao = "Desenvolvedora de Software"
+objetivo = '''Aprender a programar em Python e se tornar uma desenvolvedora de software.'''
+
+# Declare variáveis de cada tipo numérico
+idade = 25  # Inteiro (int)
+altura = 1.68  # Ponto flutuante (float)
+peso = 60.5  # Ponto flutuante (float)
+numero_complexo = 2 + 3j  # Número complexo (complex)
+
+# Declare uma variável do tipo booleano
+aprendendo_python = True
+
+# Exibir os valores
+print(nome)
+print(profissao)
+print(objetivo)
+print(idade)
+print(altura)
+print(peso)
+print(numero_complexo)
+print(aprendendo_python)
+
