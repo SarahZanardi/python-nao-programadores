f3e2469757c5216253deda9c902e7263b32140cb
Sarah Zanardi <sarahzanardi6@gmail.com>
2025-03-14 15:43:25 -0300

    Operadores.py

---
+++ Operadores.py
@@ -0,0 +1,11 @@
+ano_nascimento = 1989
+ano_formatura = 2010
+
+# Calcular a idade de Gerlaine no ano da formatura
+idade_formatura = ano_formatura - ano_nascimento
+print(f"A idade de Gerlaine no ano da formatura foi {idade_formatura} anos.")
+
+# Comparações com operadores relacionais
+print(ano_formatura > ano_nascimento)  # ano_formatura é maior que ano_nascimento? (True ou False)
+print(ano_formatura <= ano_nascimento)  # ano_formatura é menor ou igual a ano_nascimento? (True ou False)
+print(ano_formatura == ano_nascimento)  # ano_formatura é igual a ano_nascimento? (True ou False)
