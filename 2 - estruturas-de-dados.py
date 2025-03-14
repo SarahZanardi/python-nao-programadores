f320488139a70e0535b63422968b007d1cba2067
Sarah Zanardi <sarahzanardi6@gmail.com>
2025-03-14 15:55:12 -0300

    2 - estruturas-de-dados.py

---
+++ 2 - estruturas-de-dados.py
@@ -0,0 +1,10 @@
+ano_nascimento = 1989
+ano_formatura = 2010
+
+# Calcular idade
+print(ano_formatura - ano_nascimento)
+
+# Comparações
+print(ano_formatura > ano_nascimento)
+print(ano_formatura <= ano_nascimento)
+print(ano_formatura == ano_nascimento)
