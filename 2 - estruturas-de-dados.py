a24b8f8274e25d2b71998bcf2c5c90b63f18c612
Sarah Zanardi <sarahzanardi6@gmail.com>
2025-03-14 15:57:42 -0300

    2 - estruturas-de-dados.py
    Exercício 1 da Lista 2.

--- 2 - estruturas-de-dados.py
+++ 2 - estruturas-de-dados.py
@@ -1,10 +1,17 @@
+# Definindo os anos de nascimento e formatura
 ano_nascimento = 1989
 ano_formatura = 2010
 
-# Calcular idade
-print(ano_formatura - ano_nascimento)
+# Calcular e imprimir a idade de Gerlaine no ano da formatura
+print(ano_formatura - ano_nascimento)  # A idade é a diferença entre o ano de formatura e o ano de nascimento
 
-# Comparações
+# Comparações com operadores relacionais:
+# Verifica se o ano de formatura é maior que o ano de nascimento
 print(ano_formatura > ano_nascimento)
+
+# Verifica se o ano de formatura é menor ou igual ao ano de nascimento
 print(ano_formatura <= ano_nascimento)
+
+# Verifica se o ano de formatura é igual ao ano de nascimento
 print(ano_formatura == ano_nascimento)
+
